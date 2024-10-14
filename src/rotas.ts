122f3db354cb4e7e8c225a7ab421e854a2ce1008
Pedro Gaya <pedro.gaya@cubos.academy>
2024-10-14 14:30:17 -0300

    chore: get / returns 200

--- src/rotas.ts
+++ src/rotas.ts
@@ -3,7 +3,7 @@ import { Request, Response, Router } from "express";
 const rotas = Router();
 
 rotas.get('/', (req: Request, res: Response) => {
-    return res.status(201).json({mensagem: "API de vendas de ingressos"})
+    return res.status(200).json({mensagem: "API de vendas de ingressos"})
 })
 
 export default rotas;
